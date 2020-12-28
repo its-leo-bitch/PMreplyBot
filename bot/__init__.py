68b8c2435e06df27594db73c5e91ab05550f8ea2
Call me...leo <74614943+its-leo-bitch@users.noreply.github.com>
2020-12-28 16:34:14 +0530

    Updated..

--- bot/__init__.py
+++ bot/__init__.py
@@ -62,26 +62,17 @@ START_COMMAND = get_config("START_COMMAND", "start")
 START_OTHER_USERS_TEXT = get_config(
     "START_OTHER_USERS_TEXT",
     (
-        "Hi. ☺️\n"
-        "Thank you for using me 😬\n\n"
-        "This is an Open Source Project available on "
-        "https://github.com/SpEcHIDe/NoPMsBot\n\n\n"
-        "If you are the owner of this bot, "
-        "and are seeing this message 🤦‍♂️, "
-        "means that you have not set up "
-        "the ENVironment variables properly "
-        "for the bot to function.\n\n\n"
-        "ℹ️ Subscribe @SpEcHlDe if you 😍 using this bot❗️❣️"
+        "Yo..I'm Leo's Personal Manager.*{}*"
+        "My Brain..was created by [{}](tg://user?id={}) "
+        "
     )
 )
 # check online status of your bot
 ONLINE_CHECK_START_TEXT = get_config(
     "ONLINE_CHECK_START_TEXT",
     (
-        "i am online <b>master</b>\n\n"
-        "This is an Open Source Project available on "
-        "https://github.com/SpEcHiDe/NoPMsBot\n\n\n"
-        "ℹ️ Subscribe @SpEcHlDe if you 😍 using this bot❗️❣️"
+        "Master's BITCH Is online <b>master</b>\n\n"
+        
     )
 )
 # IDEKWBYRW
